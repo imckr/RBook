ef34a69a281fca2c04c7e96907dace73364375d7
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2024-09-25 23:47:14 +0530

    92524

---
+++ CoinsProblem.rs
@@ -0,0 +1,24 @@
+fn main() {
+
+    // 5, 10, 20
+
+    let greed = [95, 20, 30, 25, 55];
+
+    // [20, 25, 30, 55, 90]
+
+    let five = 0;
+    let ten = 0;
+    let twenty = 0;
+
+    let mut sorted_greed = greed.to_vec();
+    sorted_greed.sort();
+    // println!("Sorted greed: {:?}", sorted_greed);
+
+    for coin in sorted_greed {
+        if (coin%5 == 0) {
+            
+            }
+        }
+    }
+
+}
